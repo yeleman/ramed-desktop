0ee4db6c438dd0c8c879be440ae3937c2004dfa9
renaud gaudin <rgaudin@gmail.com>
2016-07-10 20:13:24 +0000

    removed logging on win exe

--- app_logging.py
+++ app_logging.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+import sys
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+# disable logging on Windows exe
+if not hasattr(sys, 'frozen'):
+    logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('RAMED')
